5ffc60c1c24119cf8378856bee9f97b708ec1c0e
Jhennifer Kelly Nicolau da Cunha <132682121+JhenniferK@users.noreply.github.com>
2025-05-27 21:49:37 -0300

    Correção na tela inicial

--- src/components/TelaInicial/LoginCadastro.jsx
+++ src/components/TelaInicial/LoginCadastro.jsx
@@ -1,6 +1,6 @@
 import './LoginCadastro.css';
-import logonome from '../assets/logo-cuidarme(nome).png'
-import psicologa from '../assets/psicologa.png'
+import logonome from '../../assets/logo-cuidarme(nome).png'
+import psicologa from '../../assets/psicologa.png'
 
 const LoginCadastro = () => {
   return (
@@ -17,14 +17,14 @@ const LoginCadastro = () => {
       <main className="conteudo">
         <div className="texto">
           <h1>Cuidando da sua<br></br>saúde mental</h1>
-          <p>Olá, psicólogo! Faça login ou cadastre-se para começar a usar o Cuidar.me.</p>
+          <p>Faça login ou cadastre-se para começar a usar o Cuidar.me.</p>
           <div className="botoes">
             <button className="btn">Login</button>
             <button className="btn btn-secundario">Cadastrar</button>
           </div>
         </div>
         <div className="imagem">
-          <img src={psicologa} alt="Personagem psi" />
+          <img src={psicologa} alt="Psicologa" />
         </div>
       </main>
     </div>
