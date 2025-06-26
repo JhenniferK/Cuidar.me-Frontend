a090de6199d7e31368df354326fdcb52bfd9843f
Jhennifer Kelly Nicolau da Cunha <132682121+JhenniferK@users.noreply.github.com>
2025-06-26 19:55:41 -0300

    Atualização na TelaSobre

--- src/components/TelaInicial/TelaSobre/Sobre.jsx
+++ src/components/TelaInicial/TelaSobre/Sobre.jsx
@@ -19,7 +19,7 @@ const Sobre = () => {
       <main className="conteudo-sobre">
         <div className="texto-sobre">
           <h1>Sobre</h1>
-          <p>O Cuidar.me é um sistema web que está sendo desenvolvido<br></br>para otimizar a rotina administrativa de psicólogos. Em um cenário<br></br>onde o excesso de tarefas manuais pode comprometer a qualidade<br></br>do atendimento, o sistema surge como uma solução completa para<br></br>organização de prontuários, agendamento de consultas, controle<br></br>financeiro, comunicação com pacientes e gestão de documentos,<br></br>tudo isso com foco em segurança e conformidade com a Lei Geral<br></br>de Proteção de Dados (nº 13.709/2018).</p>
+          <p>O Cuidar.me é um sistema web que está sendo desenvolvido<br></br>para otimizar a rotina administrativa de psicólogos. Em um<br></br>cenário onde o excesso de tarefas manuais pode comprometer<br></br>a qualidade do atendimento, o sistema surge como uma solução<br></br>completa para organização de prontuários, agendamento<br></br>de consultas, controle financeiro, comunicação com pacientes e<br></br>gestão de documentos, tudo isso com foco em segurança e<br></br>conformidade com a Lei Geral de Proteção de Dados (nº 13.709/2018).</p>
         </div>
         <div className="imagem-sobre">
           <img src={psicologa} alt="Psicologa" />
